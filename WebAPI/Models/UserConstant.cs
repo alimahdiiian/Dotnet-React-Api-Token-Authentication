b86f12827bc800cb0cf5fbd92e1755fb1e9df5ab
schahrad <alimahdiiian@gmail.com>
2024-05-09 15:01:46 +0200

    add model classes

---
+++ WebAPI/Models/UserConstant.cs
@@ -0,0 +1,30 @@
+﻿namespace WebAPI.Models
+{
+    public class UserConstant
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel()
+            {
+                    UserName = "John_Doei",
+                 EmailAddress = "John@gmail.com",
+                  PassWord= "John2024",
+                   GivenName="John",
+                    Role="Administrator",
+                     SureName="Doei"              
+
+            },
+
+            new UserModel()
+            {
+               UserName = "Schahraad",
+                 EmailAddress = "schahrad@gmail.com",
+                  PassWord= "Schahrad2024",
+                   GivenName="schahrad",
+                    Role="student",
+                     SureName="mahidan"
+            }
+
+        };
+    }
+}
