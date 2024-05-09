2d6614bd74a99588a1dace2ca39ce519ba65b890
schahrad <alimahdiiian@gmail.com>
2024-05-09 17:55:17 +0200

    add login controller to Authenicate user & generate token

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPI.Models;
+
+
+//Authenticate the user & generate Token 
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+
+            }
+
+            return NotFound("user not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+            var credentials =
+                new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim (ClaimTypes.Surname, user.SureName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            // Create JWT token
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15), // Token expiration time
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstant.Users.FirstOrDefault(u => u.UserName
+            .ToLower() == userLogin.UserName.ToLower()
+            && u.PassWord == userLogin.PassWord);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+
+
+
+
+    }
+}
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98102ea38034c069c09843eb740ce09e85fbd897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86f12827bc800cb0cf5fbd92e1755fb1e9df5ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
